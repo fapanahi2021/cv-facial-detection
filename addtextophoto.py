b544ba761b0549499844f045f2f682e2fb4f6350
fapanahi2021 <94350619+fapanahi2021@users.noreply.github.com>
2022-05-21 07:47:28 +0430

    Add files via upload

---
+++ addtextophoto.py
@@ -0,0 +1,36 @@
+#imported the opencv libray
+import cv2
+
+#add an image
+image = 'test4.jpg'
+
+#read the image
+img =  cv2.imread(image)
+
+#show the image
+cv2.imshow("test4", img)
+
+#add delay to closing the image
+cv2.waitKey(0)
+#resize the image
+new_dimension = (100, 100)
+img_resized = cv2.resize(img, new_dimension)
+
+height, width, depth = img.shape
+desired_height = 512
+aspect_ratio = desired_height/width
+dimension = (desired_height, height*aspect_ratio)
+img_resized = cv2.resize(img, dimension)
+
+#add text to image
+BLACK = (255,255,255)
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_size = 1.1
+font_color = BLACK
+font_thickness = 2
+text = 'Taghipanahi'
+x,y = 10,650
+img_text = cv2.putText(img_resized, text, (x,y), font, font_size, font_color, font_thickness, cv2.LINE_AA)
+
+cv2.imwrite('test4_resized_text.jpg',img_text)
+
