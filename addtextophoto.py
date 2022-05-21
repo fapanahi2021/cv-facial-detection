571b209d386a63cf3b18de1158040d6bdd5ce464
fapanahi2021 <94350619+fapanahi2021@users.noreply.github.com>
2022-05-21 08:03:25 +0430

    Update addtextophoto.py

--- addtextophoto.py
+++ addtextophoto.py
@@ -1,18 +1,19 @@
-#imported the opencv libray
+#imported the opencv libray, وارد کردن کتابخانه مورد نظر
 import cv2
 
-#add an image
+#add an image, افزودن تصویر
 image = 'test4.jpg'
 
-#read the image
+#read the image, خواندن تصویر
 img =  cv2.imread(image)
 
-#show the image
+#show the image, نشان دادن تصویر
 cv2.imshow("test4", img)
 
-#add delay to closing the image
+#add delay to closing the image, ایجاد وقفه
 cv2.waitKey(0)
-#resize the image
+
+#resize the image, تغییر سایز تصویر
 new_dimension = (100, 100)
 img_resized = cv2.resize(img, new_dimension)
 
@@ -22,7 +23,7 @@ aspect_ratio = desired_height/width
 dimension = (desired_height, height*aspect_ratio)
 img_resized = cv2.resize(img, dimension)
 
-#add text to image
+#add text to image, اضافه کردن متن به تصویر
 BLACK = (255,255,255)
 font = cv2.FONT_HERSHEY_SIMPLEX
 font_size = 1.1
